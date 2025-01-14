a44e5496683b0ada2bc21c60b0a44e600c898d39
DOPEgduranti <guglielmo.duranti10@gmail.com>
2025-01-14 11:10:13 +0100

    bellino

--- main.js
+++ main.js
@@ -6,14 +6,41 @@ import { Group, remove } from 'three/examples/jsm/libs/tween.module.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 // camera.position.z = 5;
-let look_x = 0;
-let look_y = 0;
-let look_z = 0;
-let camera_x = 0;
-let camera_y = 0;
-let camera_distance = 100;
-camera.position.set(camera_x,camera_y, camera_distance );
-camera.lookAt( look_x,look_y,look_z );
+let look = {
+	x : 0,
+	y : 0,
+	z : 0
+}
+
+let cam = {
+	x : 0,
+	y : 0,
+	z : 100
+}
+
+let ring = {
+	x : 0,
+	y : 150,
+	z : 10,
+	h : 2
+}
+ring.x = (9/16 * ring.y) - ring.h;
+
+let player = {
+	y : ring.x / 6,
+	h : 2,
+	z : 5
+}
+
+let mat = {
+	ring : new THREE.MeshStandardMaterial( {color: 'blue', emissive: 'red', metalness: 1, roughness: 0.5} ),
+	player : new THREE.MeshNormalMaterial(),
+	ball : new THREE.MeshStandardMaterial( {color: 'red', emissive: 'blue', metalness: 1, roughness: 0.5} )
+}
+
+
+camera.position.set(cam.x,cam.y, cam.z );
+camera.lookAt( look.x,look.y,look.z );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth,window.innerHeight );
@@ -21,47 +48,24 @@ renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 
-// const l_material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
-// const top_points = [];
-// top_points.push( new THREE.Vector3( -45, 30, 10 ) );
-// top_points.push( new THREE.Vector3( 45, 30, 10 ) );
-// const top_geometry = new THREE.BufferGeometry().setFromPoints( top_points );
-// const top = new THREE.Line( top_geometry, l_material );
-// scene.add( top );
-let ring_y = 150;
-let ring_z = 10;
-let ring_h = 2;
-let ring_x = (9/16 * ring_y) - ring_h;
-let player_y = ring_x / 6;
-let player_h = 2;
-let player_z = 5;
-let ball_color = 0x00ff00;
-let ball_radius = ring_y / 80;
-
-const r_bottom = new THREE.Mesh(new THREE.BoxGeometry(ring_y, ring_h, ring_z),
-	new THREE.MeshPhysicalMaterial( {color: 'red'} ));
-const r_top = new THREE.Mesh(new THREE.BoxGeometry(ring_y, ring_h, ring_z),
-	new THREE.MeshPhysicalMaterial());
-const r_left = new THREE.Mesh(new THREE.BoxGeometry(ring_h, ring_x, ring_z),
-	new THREE.MeshPhysicalMaterial());
-const r_right = new THREE.Mesh(new THREE.BoxGeometry(ring_h, ring_x, ring_z),
-	new THREE.MeshPhysicalMaterial());
+const r_bottom = new THREE.Mesh(new THREE.BoxGeometry(ring.y, ring.h, ring.z),mat.ring);
+const r_top = new THREE.Mesh(new THREE.BoxGeometry(ring.y, ring.h, ring.z), mat.ring);
+const r_left = new THREE.Mesh(new THREE.BoxGeometry(ring.h, ring.x, ring.z), mat.ring);
+const r_right = new THREE.Mesh(new THREE.BoxGeometry(ring.h, ring.x, ring.z), mat.ring);
 
 
-r_bottom.position.set(0,-((ring_x + ring_h) / 2),0);
-r_top.position.set(0,((ring_x + ring_h) / 2),0);
-r_left.position.set(-((ring_y - ring_h) / 2),0,0);
-r_right.position.set(((ring_y - ring_h) / 2),0,0);
-const ring = new THREE.Group();
-ring.add(r_bottom, r_top, r_left, r_right);
+r_bottom.position.set(0,-((ring.x + ring.h) / 2),0);
+r_top.position.set(0,((ring.x + ring.h) / 2),0);
+r_left.position.set(-((ring.y - ring.h) / 2),0,0);
+r_right.position.set(((ring.y - ring.h) / 2),0,0);
+const ring3D = new THREE.Group();
+ring3D.add(r_bottom, r_top, r_left, r_right);
 
-const p1 = new THREE.Mesh(new THREE.BoxGeometry(player_h,player_y ,player_z), 
-new THREE.MeshNormalMaterial());
-const p2 = new THREE.Mesh(new THREE.BoxGeometry(player_h,player_y ,player_z), 
-new THREE.MeshNormalMaterial());
-p1.position.set(-(ring_y * 2/5),0,0);
-p2.position.set((ring_y * 2/5),0,0);
+const p1 = new THREE.Mesh(new THREE.BoxGeometry(player.h,player.y ,player.z), mat.player);
+const p2 = new THREE.Mesh(new THREE.BoxGeometry(player.h,player.y ,player.z), mat.player);
+p1.position.set(-(ring.y * 2/5),0,0);
+p2.position.set((ring.y * 2/5),0,0);
 
 let p1_score = 0;
 let p2_score = 0;
@@ -69,9 +73,9 @@ let p2_score = 0;
 let p1_move_y = 0;
 let p2_move_y = 0;
 
-const m = new THREE.MeshStandardMaterial({color: 'red'});
-const ball = new THREE.Mesh( new THREE.SphereGeometry( ball_radius ), m );
-let ball_speed = ring_y/150;
+let ball_radius = ring.y / 80;
+let ball_speed = ring.y/150;
+const ball = new THREE.Mesh( new THREE.SphereGeometry( ball_radius ), mat.ball );
 let angle =  Math.floor(Math.random() * 70);
 if (angle % 2)
 	angle *= -1;
@@ -87,7 +91,7 @@ dirLight.position.set( 10, 7, 40 ).normalize();
 scene.add( dirLight );
 
 const game = new THREE.Group();
-game.add(ring, p1, p2, ball);
+game.add(ring3D, p1, p2, ball);
 scene.add(game);
 refresh_score();
 
@@ -106,53 +110,53 @@ function animate() {
 	// game.rotation.x += 0.01;
 	// game.rotation.z += 0.01;
 	
-	if ((ball.position.x - ball_radius - ball_speed < p1.position.x + player_h / 2)
-		&& (ball.position.x - ball_radius > p1.position.x - player_h / 2)
-		&& (ball.position.y - ball_radius < p1.position.y + player_y / 2)
-		&& (ball.position.y + ball_radius > p1.position.y - player_y / 2)) {
+	if ((ball.position.x - ball_radius - ball_speed < p1.position.x + player.h / 2)
+		&& (ball.position.x - ball_radius > p1.position.x - player.h / 2)
+		&& (ball.position.y - ball_radius < p1.position.y + player.y / 2)
+		&& (ball.position.y + ball_radius > p1.position.y - player.y / 2)) {
 		hit_position = (ball.position.y - p1.position.y);
 		p1_hit = 1;
-		angle = hit_position / (player_h,player_y) * -90;
+		angle = hit_position / (player.h,player.y) * -90;
 	} //p1
-	else if	((ball.position.x + ball_radius + ball_speed > p2.position.x - player_h / 2 )
-		&& (ball.position.x + ball_radius < p2.position.x + player_h / 2)
-		&& (ball.position.y + ball_radius > p2.position.y - player_y / 2)
-		&& (ball.position.y - ball_radius < p2.position.y + player_y / 2)) {
+	else if	((ball.position.x + ball_radius + ball_speed > p2.position.x - player.h / 2 )
+		&& (ball.position.x + ball_radius < p2.position.x + player.h / 2)
+		&& (ball.position.y + ball_radius > p2.position.y - player.y / 2)
+		&& (ball.position.y - ball_radius < p2.position.y + player.y / 2)) {
 		hit_position = (ball.position.y - p2.position.y);
 		p2_hit = 1;
-		angle = 180 + (hit_position / (player_h,player_y) * 90);
+		angle = 180 + (hit_position / (player.h,player.y) * 90);
 	}
 	if (p1_hit || p2_hit) {
-	if (ball_speed < 5 * player_h )
+	if (ball_speed < 5 * player.h )
 		ball_speed += 0.1;
 		p1_hit = 0;
 		p2_hit = 0;		
 	}
-	if ((ball.position.y + ball_radius > ring_x / 2)
-		|| (ball.position.y - ball_radius < -ring_x / 2 ))
+	if ((ball.position.y + ball_radius > ring.x / 2)
+		|| (ball.position.y - ball_radius < -ring.x / 2 ))
 		// || (ball.position.y + ball_radius > p1.position.y)
-		// && (ball.position.x - ball_radius < p1.position.x - player_h / 2)
-		// && (ball.position.x + ball_radius > p1.position.x + player_h / 2)) //muro orizzontale
+		// && (ball.position.x - ball_radius < p1.position.x - player.h / 2)
+		// && (ball.position.x + ball_radius > p1.position.x + player.h / 2)) //muro orizzontale
 		{
 			angle *= -1;
 		}
-	else if (ball.position.x - ball_radius < r_left.position.x + ring_h) {
+	else if (ball.position.x - ball_radius < r_left.position.x + ring.h) {
 		console.log("p2 ha segnato");
 		p2_score += 1;
 		restart_game();
 	}
-	else if (ball.position.x + ball_radius > r_right.position.x - ring_h) {
+	else if (ball.position.x + ball_radius > r_right.position.x - ring.h) {
 		console.log("p1 ha segnato");
 		p1_score += 1;
 		restart_game();
 	}
 	ball.position.y += ball_speed * -Math.sin(angle * Math.PI /180);
 	ball.position.x += ball_speed * Math.cos(angle * Math.PI /180);
-	if ((p1_move_y > 0 && p1.position.y < ring_x / 2 - player_y / 2) 
-		|| (p1_move_y < 0 && p1.position.y > - ring_x / 2 + player_y / 2))
+	if ((p1_move_y > 0 && p1.position.y < ring.x / 2 - player.y / 2) 
+		|| (p1_move_y < 0 && p1.position.y > - ring.x / 2 + player.y / 2))
 		p1.position.y += p1_move_y;
-	if ((p2_move_y > 0 && p2.position.y < ring_x / 2 - player_y / 2)
-		|| (p2_move_y < 0 && p2.position.y > - ring_x / 2 + player_y / 2))
+	if ((p2_move_y > 0 && p2.position.y < ring.x / 2 - player.y / 2)
+		|| (p2_move_y < 0 && p2.position.y > - ring.x / 2 + player.y / 2))
 		p2.position.y += p2_move_y;
 
 	renderer.render( scene, camera );
@@ -167,32 +171,14 @@ document.addEventListener("keydown", function(event) {
 		restart_game();
 	}
 	if (event.key.toLowerCase() == 'w')
-		p1_move_y = ring_y/125;
+		p1_move_y = ring.y/125;
 	if (event.key.toLowerCase() == 's')
-		p1_move_y = -ring_y/125;
+		p1_move_y = -ring.y/125;
 	if (event.key == 'ArrowUp')
-		p2_move_y = ring_y/125;
+		p2_move_y = ring.y/125;
 	if (event.key == 'ArrowDown')
-		p2_move_y = -ring_y/125;
+		p2_move_y = -ring.y/125;
 		console.log(event);
-	if (event.key == '8')
-		look_y +=1;
-	if (event.key == '2')
-		look_y -=1;
-	if (event.key == '6')
-		look_x +=1;
-	if (event.key == '4')
-		look_x -=1;
-	if (event.key == '9')
-		camera_x += 1
-	if (event.key == '7')
-		camera_x -= 1
-	if (event.key == '3')
-		camera_y += 1
-	if (event.key == '1')
-		camera_y -= 1
-	camera.position.set(camera_x,camera_y, camera_distance );
-	camera.lookAt( look_x,look_y,look_z )
   });
 
   document.addEventListener("keyup", function(event) {
@@ -217,20 +203,20 @@ document.addEventListener("keydown", function(event) {
 	scene.remove(scene.getObjectByName('txt'));
 	refresh_score();
 	ball.position.set(0, 0, 0);
-	ball_speed = 1;
-	p1.position.set(-(ring_y * 2/5),0,0);
-	p2.position.set((ring_y * 2/5),0,0);
+	ball_speed = ring.y / 150;
+	p1.position.set(-(ring.y * 2/5),0,0);
+	p2.position.set((ring.y * 2/5),0,0);
 	angle =  Math.floor(Math.random() * 70);
 	if (angle % 2)
 		angle *= -1;
 	if (angle % 3)
 		angle += 180;
-	camera_x = 0;
-	camera_y = 0;
-	look_x = 0;
-	look_y = 0;
-	look_z = 0;
-	camera.position.set(0,0, camera_distance );
+	cam.x = 0;
+	cam.y = 0;
+	look.x = 0;
+	look.y = 0;
+	look.z = 0;
+	camera.position.set(0,0, cam.z );
 	camera.lookAt( 0,0,0 )
   }
 
@@ -256,7 +242,7 @@ function refresh_score() {
 		} );
 		const txt = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial());
 		txt.name = 'txt';
-		txt.position.set(-12.4,ring_y / 3,0);
+		txt.position.set(-12.4,ring.y / 3,0);
 		console.log( font );
 		scene.add(txt);
 		game.add(txt);
