7e08ed0f5a3d34fcec343a8cfdbc3a55831ce333
sim1gariglio <simone.gariglio1@gmail.com>
2025-01-23 05:23:41 -0500

    post  method, p1score and p2score

--- main.js
+++ main.js
@@ -324,6 +324,7 @@ function game_over() {
 	document.getElementById('gameOverImage').style.display = 'block';
 	const winner = p1_score >= maxScore ? 'Player 1' : 'Player 2';
     createWinnerText(winner);
+	sendScore(p1_score, p2_score);
     showMainMenu();
 }
 
@@ -533,3 +534,42 @@ function exitGame() {
 }
 
 document.getElementById('menu').style.display = 'block';
+
+function sendScore(player1Score, player2Score) {
+    fetch('/api/score/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': getCookie('csrftoken') // Include CSRF token for security
+			// JWT o OAUTH token 
+        },
+        body: JSON.stringify({
+            player1Score: player1Score,
+            player2Score: player2Score
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+// Function to get CSRF token from cookies
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
